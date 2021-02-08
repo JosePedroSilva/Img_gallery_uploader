91060125cc6fc9f8bf4ab5b122e43cdd8e5cc58d
Jose Pedro <josepedro88@live.com.pt>
2021-02-08 14:10:47 +0000

    upload file progress bar

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/storage";
 import "firebase/firebase";
+import "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -15,7 +16,6 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();
 
 const projectStorage = firebase.storage();
 const projectFirestore = firebase.firestore();
